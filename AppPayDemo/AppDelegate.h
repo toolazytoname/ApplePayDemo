0ae59a5ae64c1ad6ddb52fde2f465b8a6c83e771
韦超(无线运营中心) <chaowei@sohu-inc.com>
2016-03-09 12:06:11 +0800

    Initial Commit

---
+++ AppPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppPayDemo
+//
+//  Created by weichao on 16/3/9.
+//  Copyright © 2016年 weichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppPayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppPayDemo
+//
+//  Created by weichao on 16/3/9.
+//  Copyright © 2016年 weichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
